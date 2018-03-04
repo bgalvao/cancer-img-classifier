b6f941335ac2542e9d124f68252f695cb417e185
bgalvao <bernardogcgalvao@gmail.com>
2018-03-04 20:12:24 +0000

    init commit

---
+++ data_download.sh
@@ -0,0 +1,7 @@
+wget https://www.kaggle.com/paultimothymooney/breast-histopathology-images/downloads/breast-histopathology-images.zip/1
+unzip *zip
+rm breast*zip
+mkdir data
+mv *zip data/
+cd data && unzip *zip
+rm IDC*zip
